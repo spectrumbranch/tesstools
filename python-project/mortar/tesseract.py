28ff83823213b2d4c930288e7f201d43e0e9db93
Fritz Mahnke <fritz@mahnke.tech>
2024-06-11 17:03:41 -0700

    tesseract runner uses config/SSH

--- python-project/mortar/tesseract.py
+++ python-project/mortar/tesseract.py
@@ -1,8 +1,12 @@
 import os
+from pathlib import PurePath
+import shlex
 import sys
 
+from mortar.config import config
 from mortar.path import win_from_wsl
 import mortar.process as process
+from mortar.ssh import SSH
 
 _tess_env = {
     'command': os.environ['TESSERACT'],
@@ -18,9 +22,31 @@ _tess_cmd = [
 ]
 
 
-def ocr(path_) -> str:
-    """ Generate OCR text from an image in the same way MORT does. """
+def tesseract_ssh(path_: str) -> str:
+    tess_cmd = (f"{_tess_env['command']}"
+                f" -l jpn --tessdata-dir {shlex.quote(_tess_env['data'])}")
 
+    ssh = SSH(host=config.ssh.host, port=config.ssh.port)
+    path = PurePath(path_)
+
+    temp_win = "C:\\Users\\ChillRuns\\AppData\\Local\\Temp"
+    temp_nix = '/mnt/c/Users/ChillRuns/AppData/Local/Temp'
+
+    ssh.scp_to(path, f'{temp_nix}/{path.name}')
+    ssh.run([tess_cmd + f' "{temp_win}\\{path.name}" "{temp_win}\\out"'])
+    ssh.scp_from(f'{temp_nix}/out.txt', '.')
+
+    out_name = 'out.txt'
+
+    with open(out_name, 'r') as fi:
+        result = fi.read()
+
+    os.remove(out_name)
+
+    return result
+
+
+def tesseract_wsl(path_: str) -> str:
     path = win_from_wsl(path_)
     out_stem = 'out'
     out_name = f'{out_stem}.txt'
@@ -31,6 +57,17 @@ def ocr(path_) -> str:
         result = fi.read()
 
     os.remove(out_name)
+
+    return result
+
+
+def ocr(path: str) -> str:
+    """ Generate OCR text from an image in the same way MORT does. """
+
+    if config.ssh.use_ssh:
+        result = tesseract_ssh(path)
+    else:
+        result = tesseract_wsl(path)
 
     return result
 
