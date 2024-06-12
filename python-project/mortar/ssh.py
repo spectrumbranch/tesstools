e36de49b47a8f21de1c6a597cb09bf99848706b6
Fritz Mahnke <fritz@mahnke.tech>
2024-06-11 17:03:23 -0700

    build SSH commands from config

--- python-project/mortar/ssh.py
+++ python-project/mortar/ssh.py
@@ -1,26 +1,55 @@
+from enum import Enum, auto
+
 from mortar import process
 
 
+class Command(Enum):
+    SSH = auto()
+    SCP = auto()
+
+
 class SSH:
-    def __init__(self, host=None, port=None):
+    def __init__(self, host=None, port=None) -> None:
         self.host = host
         self.port = port
 
     def scp_to(self, local, remote):
-        cmd = ['scp', '-P', self.port, local, f'{self.host}:/{remote}']
+        args = self._build_args(Command.SCP, self.port)
+
+        cmd = ['scp'] + args + [local, f'{self.host}:/{remote}']
 
         result = process.run(cmd)
 
         return result
 
     def scp_from(self, remote, local):
-        cmd = ['scp', '-P', self.port, f'{self.host}:/{remote}', local]
+        args = self._build_args(Command.SCP, self.port)
+
+        cmd = ['scp'] + args + [f'{self.host}:/{remote}', local]
 
         result = process.run(cmd)
 
         return result
 
     def run(self, command):
-        ssh_command = ['ssh', '-p', self.port, self.host] + command
+        args = self._build_args(Command.SSH, self.port)
+
+        ssh_command = ['ssh'] + args + [self.host] + command
 
         return process.run(ssh_command)
+
+    @staticmethod
+    def _build_args(command, port):
+        args = []
+
+        if port is not None:
+            if command == Command.SSH:
+                args.append('-p')
+            elif command == Command.SCP:
+                args.append('-P')
+            else:
+                raise Exception()
+
+            args.append(str(port))
+
+        return args
